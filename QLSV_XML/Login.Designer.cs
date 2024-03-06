91b639123e549744dfd2ceecb10ecfde9e57b267
VUTRINGOC <vungocss88@gmail.com>
2024-03-06 16:04:41 +0700

    update

--- QLSV_XML/Login.Designer.cs
+++ QLSV_XML/Login.Designer.cs
@@ -49,35 +49,39 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Niagara Solid", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(625, 147);
+            this.label1.Location = new System.Drawing.Point(833, 181);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 34);
+            this.label1.Size = new System.Drawing.Size(170, 43);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ready to Login";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(592, 234);
+            this.textBox1.Location = new System.Drawing.Point(789, 288);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(267, 35);
+            this.textBox1.Size = new System.Drawing.Size(355, 42);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(592, 317);
+            this.textBox2.Location = new System.Drawing.Point(789, 390);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(267, 30);
+            this.textBox2.Size = new System.Drawing.Size(355, 36);
             this.textBox2.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(561, 388);
+            this.button1.Location = new System.Drawing.Point(748, 478);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(298, 43);
+            this.button1.Size = new System.Drawing.Size(397, 53);
             this.button1.TabIndex = 4;
             this.button1.Text = "ĐĂNG NHẬP";
             this.button1.UseVisualStyleBackColor = true;
@@ -87,28 +91,31 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(558, 213);
+            this.label2.Location = new System.Drawing.Point(744, 262);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.Size = new System.Drawing.Size(70, 22);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Tài Khoản";
+            this.label2.Text = "ddddd";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(558, 296);
+            this.label3.Location = new System.Drawing.Point(744, 364);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 18);
+            this.label3.Size = new System.Drawing.Size(106, 22);
             this.label3.TabIndex = 1;
             this.label3.Text = "Mật Khẩu";
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = global::QLSV_XML.Properties.Resources._lock;
-            this.pictureBox4.Location = new System.Drawing.Point(558, 317);
+            this.pictureBox4.Location = new System.Drawing.Point(744, 390);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(33, 30);
+            this.pictureBox4.Size = new System.Drawing.Size(44, 37);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 8;
             this.pictureBox4.TabStop = false;
@@ -116,9 +123,10 @@
             // pictureBox3
             // 
             this.pictureBox3.BackgroundImage = global::QLSV_XML.Properties.Resources.user__1_;
-            this.pictureBox3.Location = new System.Drawing.Point(557, 234);
+            this.pictureBox3.Location = new System.Drawing.Point(736, 288);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(34, 34);
+            this.pictureBox3.Size = new System.Drawing.Size(45, 42);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
@@ -126,9 +134,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::QLSV_XML.Properties.Resources.students;
-            this.pictureBox2.Location = new System.Drawing.Point(592, 24);
+            this.pictureBox2.Location = new System.Drawing.Point(789, 30);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(207, 110);
+            this.pictureBox2.Size = new System.Drawing.Size(276, 135);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
@@ -136,27 +145,29 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::QLSV_XML.Properties.Resources.online_education;
-            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(487, 518);
+            this.pictureBox1.Size = new System.Drawing.Size(649, 638);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(659, 474);
+            this.button2.Location = new System.Drawing.Point(879, 583);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 29);
+            this.button2.Size = new System.Drawing.Size(140, 36);
             this.button2.TabIndex = 9;
             this.button2.Text = "Đăng Kí";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(895, 512);
+            this.ClientSize = new System.Drawing.Size(1193, 630);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
@@ -168,6 +179,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Login";
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
